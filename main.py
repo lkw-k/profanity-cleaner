326626944506b2fd8ec90a8a46e9f6b08820a5a3
lkw-k <gunwoo3971@gmail.com>
2026-05-19 21:12:44 +0900

    bot.py에 봇 작성 시작

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from profanity-cleaner!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ src/bot.py
@@ -0,0 +1,5 @@
+import os                           #  환경변수 읽기
+import discord                      # 디스코드 라이브러리
+from discord.ext import commands    # 디스코드 봇 명령어
+from dotenv import load_dotenv      # .env 파일 읽기
+
